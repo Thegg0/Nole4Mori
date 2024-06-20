11a054b8e2a72ce68faff90343df486742abdd2e
Thegg0 <93227989+Thegg0@users.noreply.github.com>
2024-06-20 12:21:17 +0200

    v 0.1.7

--- main.py
+++ main.py
@@ -11,14 +11,13 @@ def crea_db():
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
     cursor.execute('''
-    
     CREATE TABLE IF NOT EXISTS dati(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         nominativo TEXT NOT NULL,
         num_bici TEXT NOT NULL,
-        data_ini DATA NOT NULL,
-        data_fin DATA NOT NULL,
-        danni TEXT ,
+        data_ini TEXT NOT NULL,
+        data_fin TEXT NOT NULL,
+        danni TEXT,
         luce_ant TEXT NOT NULL,
         luce_post TEXT NOT NULL,
         casco TEXT NOT NULL,
@@ -27,27 +26,45 @@ def crea_db():
         rip_spray TEXT NOT NULL,
         rotelle TEXT NOT NULL,
         note TEXT NOT NULL
-        )
+    )
     ''')
-
     conn.commit()
     conn.close()
 
 
 def inserisci_dati(nominativo, num_bici, data_ini, data_fin, danni, luce_ant, luce_post, casco, lucch, seggio,
-                   rip_spray,
-                   rotelle, note):
+                   rip_spray, rotelle, note):
     conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO dati (nominativo, num_bici, data_ini, data_fin,'
-                   ' danni, luce_ant, luce_post, casco, lucch, seggio, rip_spray, rotelle, note) '
-                   'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (nominativo, num_bici, data_ini, data_fin,
-                                                                      danni,
-                                                                      luce_ant, luce_post, casco, lucch, seggio,
-                                                                      rip_spray, rotelle, note))
+    cursor.execute('INSERT INTO dati (nominativo, num_bici, data_ini, data_fin, danni, luce_ant, luce_post, casco, '
+                   'lucch, seggio, rip_spray, rotelle, note) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+                   (nominativo, num_bici, data_ini, data_fin, danni, luce_ant, luce_post, casco, lucch, seggio,
+                    rip_spray, rotelle, note))
     conn.commit()
     conn.close()
-    messagebox.showinfo("Successo", "Dati Inseriti con successo!")
+    messagebox.showinfo("Successo", "Dati inseriti con successo!")
+
+
+def aggiorna_dati(id, nominativo, num_bici, data_ini, data_fin, danni, luce_ant, luce_post, casco, lucch, seggio,
+                  rip_spray, rotelle, note):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute('UPDATE dati SET nominativo=?, num_bici=?, data_ini=?, data_fin=?, danni=?, luce_ant=?, luce_post=?,'
+                   ' casco=?, lucch=?, seggio=?, rip_spray=?, rotelle=?, note=? WHERE id=?',
+                   (nominativo, num_bici, data_ini, data_fin, danni, luce_ant, luce_post, casco, lucch, seggio,
+                    rip_spray, rotelle, note, id))
+    conn.commit()
+    conn.close()
+    messagebox.showinfo("Successo", "Dati aggiornati con successo!")
+
+
+def elimina_dati(id):
+    conn = sqlite3.connect(db_path)
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM dati WHERE id=?', (id,))
+    conn.commit()
+    conn.close()
+    messagebox.showinfo("Successo", "Dati eliminati con successo!")
 
 
 def ottieni_dati():
@@ -59,13 +76,13 @@ def ottieni_dati():
     return rows
 
 
-def mostra_finestra_inserimento():
+def mostra_finestra_inserimento(id=None, dati=None):
     def submit_data():
         nominativo = entry_nomi.get()
         num_bici = entry_num_bici.get()
         data_ini = entry_data_ini.get_date().strftime('%d-%m-%Y')
         data_fin = entry_data_fin.get_date().strftime('%d-%m-%Y')
-        danni = entry_danni.get("1.0", tk.END)
+        danni = entry_danni.get("1.0", tk.END).strip()
         luce_ant = entry_luce_ant.get()
         luce_post = entry_luce_post.get()
         casco = entry_casco.get()
@@ -73,92 +90,140 @@ def mostra_finestra_inserimento():
         seggio = entry_seggio.get()
         rip_spray = entry_rip_spray.get()
         rotelle = entry_rotelle.get()
-        note = entry_note.get("1.0", tk.END)
+        note = entry_note.get("1.0", tk.END).strip()
 
         if nominativo and num_bici and data_ini and data_fin:
+            if id:
+                aggiorna_dati(id, nominativo, num_bici, data_ini, data_fin, danni, luce_ant, luce_post, casco, lucch,
+                              seggio, rip_spray, rotelle, note)
+            else:
+                inserisci_dati(nominativo, num_bici, data_ini, data_fin, danni, luce_ant, luce_post, casco, lucch,
+                               seggio, rip_spray, rotelle, note)
 
-            inserisci_dati(nominativo, num_bici, data_ini, data_fin, danni, luce_ant, luce_post, casco,
-                           lucch, seggio, rip_spray, rotelle, note)
-
-            entry_nomi.delete(0, tk.END)
-            entry_num_bici.delete(0, tk.END)
-            entry_data_ini.set_date('')
-            entry_data_fin.set_date('')
-            entry_danni.delete(1.0, tk.END)
-            entry_luce_ant.delete(0, tk.END)
-            entry_luce_post.delete(0, tk.END)
-            entry_casco.delete(0, tk.END)
-            entry_lucch.delete(0, tk.END)
-            entry_seggio.delete(0, tk.END)
-            entry_rip_spray.delete(0, tk.END)
-            entry_rotelle.delete(0, tk.END)
-            entry_note.delete(1.0, tk.END)
+            window.destroy()
+            mostra_finestra_lista()
         else:
             messagebox.showwarning("Attenzione", "I campi non sono stati tutti compilati")
 
     window = tk.Tk()
-    window.title("Inserisci Noleggio")
+    window.title("Inserisci Noleggio" if not id else "Modifica Noleggio")
 
-    tk.Label(window, text="Nominativo:").grid(row=0, column=0)
-    entry_nomi = tk.Entry(window)
-    entry_nomi.grid(row=0, column=1)
+    # Stile minimalista e color scuri
+    style = ttk.Style(window)
+    style.configure("TLabel", font=("Helvetica", 12), background="#2e2e2e", foreground="#ffffff")
+    style.configure("TButton", font=("Helvetica", 12), background="#4a4a4a", foreground="#ffffff")
+    style.configure("TEntry", font=("Helvetica", 12), background="#3e3e3e", foreground="#ffffff")
+    style.configure("TText", font=("Helvetica", 12), background="#3e3e3e", foreground="#ffffff")
+    style.configure("TFrame", background="#2e2e2e")
 
-    tk.Label(window, text="N Bici:").grid(row=0, column=2)
-    entry_num_bici = tk.Entry(window)
-    entry_num_bici.grid(row=0, column=3)
+    window.configure(bg="#2e2e2e")
 
-    tk.Label(window, text="Da:").grid(row=1, column=0)
+    frame = ttk.Frame(window)
+    frame.grid(row=0, column=0, padx=20, pady=20)
+
+    ttk.Label(window, text="Nominativo:").grid(row=0, column=0, padx=10, pady=5)
+    entry_nomi = ttk.Entry(window)
+    entry_nomi.grid(row=0, column=1, padx=10, pady=5)
+    if dati:
+        entry_nomi.insert(0, dati[1])
+
+    ttk.Label(window, text="N Bici:").grid(row=0, column=2, padx=10, pady=5)
+    entry_num_bici = ttk.Entry(window)
+    entry_num_bici.grid(row=0, column=3, padx=10, pady=5)
+    if dati:
+        entry_num_bici.insert(0, dati[2])
+
+    ttk.Label(window, text="Da:").grid(row=1, column=0, padx=10, pady=5)
     entry_data_ini = DateEntry(window, date_pattern='dd-mm-yyyy')
-    entry_data_ini.grid(row=1, column=2)
+    entry_data_ini.grid(row=1, column=1, padx=10, pady=5)
+    if dati:
+        entry_data_ini.set_date(dati[3])
 
-    tk.Label(window, text="A:").grid(row=2, column=0)
+    ttk.Label(window, text="A:").grid(row=1, column=2, padx=10, pady=5)
     entry_data_fin = DateEntry(window, date_pattern='dd-mm-yyyy')
-    entry_data_fin.grid(row=2, column=2)
+    entry_data_fin.grid(row=1, column=3, padx=10, pady=5)
+    if dati:
+        entry_data_fin.set_date(dati[4])
 
-    tk.Label(window, text="Danni:").grid(row=3, column=0)
-    entry_danni = tk.Text(window, height=5, width=30)
-    entry_danni.grid(row=3, column=2, columnspan=3, padx=5, pady=5)
+    ttk.Label(window, text="Danni:").grid(row=2, column=0, padx=10, pady=5)
+    entry_danni = tk.Text(window, height=3, width=30, font=("Helvetica", 12), bg="#ffffff")
+    entry_danni.grid(row=2, column=1, columnspan=3, padx=10, pady=5)
+    if dati:
+        entry_danni.insert(1.0, dati[5])
 
-    tk.Label(window, text="Luce Anteriore:").grid(row=4, column=0)
-    entry_luce_ant = tk.Entry(window)
-    entry_luce_ant.grid(row=4, column=1)
+    ttk.Label(window, text="Luce Anteriore:").grid(row=3, column=0, padx=10, pady=5)
+    entry_luce_ant = ttk.Entry(window)
+    entry_luce_ant.grid(row=3, column=1, padx=10, pady=5)
+    if dati:
+        entry_luce_ant.insert(0, dati[6])
 
-    tk.Label(window, text="Luce Posteriore:").grid(row=5, column=0)
-    entry_luce_post = tk.Entry(window)
-    entry_luce_post.grid(row=5, column=1)
+    ttk.Label(window, text="Luce Posteriore:").grid(row=3, column=2, padx=10, pady=5)
+    entry_luce_post = ttk.Entry(window)
+    entry_luce_post.grid(row=3, column=3, padx=10, pady=5)
+    if dati:
+        entry_luce_post.insert(0, dati[7])
 
-    tk.Label(window, text="Casco:").grid(row=6, column=0)
-    entry_casco = tk.Entry(window)
-    entry_casco.grid(row=6, column=1)
+    ttk.Label(window, text="Casco:").grid(row=4, column=0, padx=10, pady=5)
+    entry_casco = ttk.Entry(window)
+    entry_casco.grid(row=4, column=1, padx=10, pady=5)
+    if dati:
+        entry_casco.insert(0, dati[8])
 
-    tk.Label(window, text="Lucchetto:").grid(row=7, column=0)
-    entry_lucch = tk.Entry(window)
-    entry_lucch.grid(row=7, column=1)
+    ttk.Label(window, text="Lucchetto:").grid(row=4, column=2, padx=10, pady=5)
+    entry_lucch = ttk.Entry(window)
+    entry_lucch.grid(row=4, column=3, padx=10, pady=5)
+    if dati:
+        entry_lucch.insert(0, dati[9])
 
-    tk.Label(window, text="Seggiolino:").grid(row=8, column=0)
-    entry_seggio = tk.Entry(window)
-    entry_seggio.grid(row=8, column=1)
+    ttk.Label(window, text="Seggiolino:").grid(row=5, column=0, padx=10, pady=5)
+    entry_seggio = ttk.Entry(window)
+    entry_seggio.grid(row=5, column=1, padx=10, pady=5)
+    if dati:
+        entry_seggio.insert(0, dati[10])
 
-    tk.Label(window, text="Spray Riparatore:").grid(row=9, column=0)
-    entry_rip_spray = tk.Entry(window)
-    entry_rip_spray.grid(row=9, column=1)
+    ttk.Label(window, text="Spray Riparatore:").grid(row=5, column=2, padx=10, pady=5)
+    entry_rip_spray = ttk.Entry(window)
+    entry_rip_spray.grid(row=5, column=3, padx=10, pady=5)
+    if dati:
+        entry_rip_spray.insert(0, dati[11])
 
-    tk.Label(window, text="Rotelle:").grid(row=10, column=0)
-    entry_rotelle = tk.Entry(window)
-    entry_rotelle.grid(row=10, column=1)
+    ttk.Label(window, text="Rotelle:").grid(row=6, column=0, padx=10, pady=5)
+    entry_rotelle = ttk.Entry(window)
+    entry_rotelle.grid(row=6, column=1, padx=10, pady=5)
+    if dati:
+        entry_rotelle.insert(0, dati[12])
 
-    tk.Label(window, text="Note:").grid(row=11, column=0)
-    entry_note = tk.Text(window, height=5, width=30)
-    entry_note.grid(row=11, column=1, columnspan=3, padx=5, pady=5)
+    ttk.Label(window, text="Note:").grid(row=6, column=2, padx=10, pady=5)
+    entry_note = tk.Text(window, height=3, width=30, font=("Helvetica", 12), bg="#ffffff")
+    entry_note.grid(row=6, column=3, padx=10, pady=5)
+    if dati:
+        entry_note.insert(1.0, dati[13])
 
-    tk.Button(window, text="Inserisci", command=submit_data).grid(row=12, column=0, columnspan=2)
+    ttk.Button(window, text="Inserisci" if not id else "Modifica", command=submit_data).grid(row=7, column=0,
+                                                                                             columnspan=4, pady=10)
 
     window.mainloop()
 
 
 def mostra_finestra_lista():
+    def modifica_selezione():
+        selezione = tree.selection()
+        if selezione:
+            item = tree.item(selezione)
+            valori = item['values']
+            mostra_finestra_inserimento(id=valori[0], dati=valori)
+
+    def elimina_selezione():
+        selezione = tree.selection()
+        if selezione:
+            item = tree.item(selezione)
+            valori = item['values']
+            elimina_dati(valori[0])
+            mostra_finestra_lista()
+
     window = tk.Tk()
     window.title("Lista Noleggi")
+    window.configure(bg="#f0f0f0")  # Colore di sfondo grigio chiaro
 
     columns = ("ID", "Nominativo", "N Bici", "Data Inizio", "Data Fine", "Danni", "Luce Anteriore",
                "Luce Posteriore", "Casco", "Lucchetto", "Seggiolino", "Spray Riparatore", "Rotelle", "Note")
@@ -176,18 +241,23 @@ def mostra_finestra_lista():
 
     tree.pack(fill=tk.BOTH, expand=True)
 
+    tk.Button(window, text="Modifica", command=modifica_selezione).pack(side=tk.LEFT, padx=10, pady=10)
+    tk.Button(window, text="Elimina", command=elimina_selezione).pack(side=tk.RIGHT, padx=10, pady=10)
+
     window.mainloop()
 
 
 def main():
-    # assicurarsi che il database sia creato e popolato con dati
     crea_db()
 
     root = tk.Tk()
     root.title("Nole4Mori")
+    root.configure(bg="#202020")  # Colore di fonds scour
 
-    tk.Button(root, text="Inserisci Noleggio", command=mostra_finestra_inserimento).pack(pady=10)
-    tk.Button(root, text="Lista Noleggi", command=mostra_finestra_lista).pack(pady=10)
+    tk.Label(root, text="Benvenuto su Nole4Mori", font=("Helvetica", 20), fg="white", bg="#202020").pack(pady=20)
+
+    tk.Button(root, text="Inserisci Noleggio", command=mostra_finestra_inserimento, width=20).pack(pady=10)
+    tk.Button(root, text="Lista Noleggi", command=mostra_finestra_lista, width=20).pack(pady=10)
 
     root.mainloop()
 
